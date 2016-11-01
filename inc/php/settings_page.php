e6dfda63f40b42e0262591d56ca55d801a805caf
Arthur Gareginyan <arthurgareginyan@gmail.com>
2016-11-01 20:23:20 +0300

    Plugin updated to version 1.3

--- inc/php/settings_page.php
+++ inc/php/settings_page.php
@@ -10,7 +10,7 @@ defined('ABSPATH') or die("Restricted access!");
 /**
  * Render Settings Page
  *
- * @since 1.1
+ * @since 1.3
  */
 function mshighlighter_render_submenu_page() {
 
@@ -41,10 +41,10 @@ function mshighlighter_render_submenu_page() {
 	?>
 	<div class="wrap">
 		<h2>
-            <?php _e( 'My Syntax Highlighter', 'my-syntax-highlighter' ); ?>
+            <?php _e( 'My Syntax Highlighter', MSHIGHLIGHTER_TEXT ); ?>
             <br/>
             <span>
-                <?php _e( 'by <a href="http://www.arthurgareginyan.com" target="_blank">Arthur Gareginyan</a>', 'my-syntax-highlighter' ); ?>
+                <?php _e( 'by <a href="http://www.arthurgareginyan.com" target="_blank">Arthur Gareginyan</a>', MSHIGHLIGHTER_TEXT ); ?>
             <span/>
 		</h2>
 
@@ -55,37 +55,34 @@ function mshighlighter_render_submenu_page() {
                 <div id="side-sortables" class="meta-box-sortabless ui-sortable">
 
                     <div id="about" class="postbox">
-                        <h3 class="title"><?php _e( 'About', 'my-syntax-highlighter' ); ?></a></h3>
+                        <h3 class="title"><?php _e( 'About', MSHIGHLIGHTER_TEXT ); ?></a></h3>
                         <div class="inside">
-                            <p><?php _e( 'Simple post syntax-highlighted code without losing it\'s formatting or making any manual changes. Supporting multiple languages, shortcodes and themes.', 'my-syntax-highlighter' ); ?></p>
+                            <p><?php _e( 'Simple post syntax-highlighted code without losing it\'s formatting or making any manual changes. Supporting multiple languages, shortcodes and themes.', MSHIGHLIGHTER_TEXT ); ?></p>
                         </div>
                     </div>
 
                     <div id="help" class="postbox">
-                        <h3 class="title"><?php _e( 'Help', 'my-syntax-highlighter' ); ?></h3>
+                        <h3 class="title"><?php _e( 'Help', MSHIGHLIGHTER_TEXT ); ?></h3>
                         <div class="inside">
-                            <p><?php _e( 'Got something to say? Need help?', 'my-syntax-highlighter' ); ?></p>
+                            <p><?php _e( 'Got something to say? Need help?', MSHIGHLIGHTER_TEXT ); ?></p>
                             <p><a href="mailto:arthurgareginyan@gmail.com?subject=My Syntax Highlighter">arthurgareginyan@gmail.com</a></p>
                         </div>
                     </div>
 
                     <div id="donate" class="postbox">
-                        <h3 class="title"><?php _e( 'Donate', 'my-syntax-highlighter' ); ?></h3>
+                        <h3 class="title"><?php _e( 'Donate', MSHIGHLIGHTER_TEXT ); ?></h3>
                         <div class="inside">
-                            <p><?php _e( 'If you like this plugin and find it useful, please help me to make this plugin even better and keep it up-to-date.', 'my-syntax-highlighter' ); ?></p>
+                            <p><?php _e( 'I\'m an independent developer, without a regular income, so every little contribution helps cover my costs and lets me spend more time building things for people like you to enjoy.', MSHIGHLIGHTER_TEXT ); ?></p>
                             <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS" target="_blank" rel="nofollow">
                                 <img src="<?php echo plugins_url('../img/btn_donateCC_LG.gif', __FILE__); ?>" alt="Make a donation">
                             </a>
-                            <p><?php _e( 'Thanks for your support!', 'my-syntax-highlighter' ); ?></p>
+                            <p><?php _e( 'Thanks for your support!', MSHIGHLIGHTER_TEXT ); ?></p>
                         </div>
                     </div>
 
-                    <div id="advertisement" class="postbox">
-                        <h3 class="title"><?php _e( 'Advertisement', 'my-syntax-highlighter' ); ?></h3>
-                        <div class="inside">
-                            <a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=36439_5_1_21" target="_blank" rel="nofollow"><img style="border:0px" src="http://www.elegantthemes.com/affiliates/media/banners/divi_250x250.jpg" width="250" height="250" alt="Divi WordPress Theme"></a>
-                        </div>
-                    </div>
+                    <a href="//www.iconfinder.com/?ref=ArthurGareginyan" target="_blank" rel="nofollow">
+                        <img style="border:0px" src="<?php echo plugins_url('../img/banner.png', __FILE__); ?>" width="280" height="180" alt="">
+                    </a>
 
                 </div>
             </div>
@@ -100,14 +97,14 @@ function mshighlighter_render_submenu_page() {
                             <?php settings_fields( 'mshighlighter_settings_group' ); ?>
 
                             <div class="postbox" id="Settings">
-                                <h3 class="title"><?php _e( 'Main Settings', 'my-syntax-highlighter' ); ?></h3>
+                                <h3 class="title"><?php _e( 'Main Settings', MSHIGHLIGHTER_TEXT ); ?></h3>
                                 <div class="inside">
 
                                     <table class="form-table">
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'Enable Plugin:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Enable Plugin:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="checkbox" name="mshighlighter_settings[enable]" id="mshighlighter_settings[enable]" <?php if ( !empty($options['enable']) ) { checked( $options['enable'], "on" ); } ?> >
@@ -116,13 +113,13 @@ function mshighlighter_render_submenu_page() {
                                         <tr valign='top'>
                                             <td></td>
                                             <td class='help-text'>
-                                                <?php _e( 'Enable or disable this plugin.', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Enable or disable this plugin.', MSHIGHLIGHTER_TEXT ); ?>
                                             </td>
                                         </tr>
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'Default language:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Default language:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <select name="mshighlighter_settings[defaultLanguage]">
@@ -155,13 +152,13 @@ function mshighlighter_render_submenu_page() {
                                         <tr valign='top'>
                                             <td></td>
                                             <td class='help-text'>
-                                                <?php _e( 'Default language mode for the shortcode [code]. You can select -NONE- to leave without highlighting.', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Default language mode for the shortcode [code]. You can select -NONE- to leave without highlighting.', MSHIGHLIGHTER_TEXT ); ?>
                                             </td>
                                         </tr>
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'Color theme:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Color theme:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <select name="mshighlighter_settings[theme]">
@@ -215,13 +212,13 @@ function mshighlighter_render_submenu_page() {
                                         <tr valign='top'>
                                             <td></td>
                                             <td class='help-text'>
-                                                <?php _e( 'Theme which you like to view.', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Theme which you like to view.', MSHIGHLIGHTER_TEXT ); ?>
                                             </td>
                                         </tr>
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'Display line numbers:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Display line numbers:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="checkbox" name="mshighlighter_settings[line_numbers]" id="mshighlighter_settings[line_numbers]" class="" <?php if ( !empty($options['line_numbers']) ) { checked( $options['line_numbers'], "on" ); } ?> >
@@ -230,7 +227,7 @@ function mshighlighter_render_submenu_page() {
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'First line number:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'First line number:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="text" name="mshighlighter_settings[first_line_number]" id="mshighlighter_settings[first_line_number]" size="3" value="<?php if ( !empty($options['first_line_number']) ) { echo $options['first_line_number']; } else { echo "0"; } ?>" >
@@ -239,7 +236,7 @@ function mshighlighter_render_submenu_page() {
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'The width of Tab:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'The width of Tab:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="text" name="mshighlighter_settings[tab_size]" id="mshighlighter_settings[tab_size]" size="1" value="<?php if ( !empty($options['tab_size']) ) { echo $options['tab_size']; } else { echo "4"; } ?>" >
@@ -248,7 +245,7 @@ function mshighlighter_render_submenu_page() {
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'Automatic height of code block:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'Automatic height of code block:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="checkbox" name="mshighlighter_settings[automatic_height]" id="mshighlighter_settings[automatic_height]" class="" <?php if ( !empty($options['automatic_height']) ) { checked( $options['automatic_height'], "on" ); } ?> >
@@ -258,13 +255,13 @@ function mshighlighter_render_submenu_page() {
                                         <tr valign='top'>
                                             <td></td>
                                             <td class='help-text'>
-                                                <?php _e( 'ON - Automatic height. OFF - Fixed height, with scrollbar.', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'ON - Automatic height. OFF - Fixed height, with scrollbar.', MSHIGHLIGHTER_TEXT ); ?>
                                             </td>
                                         </tr>
 
                                         <tr valign='top'>
                                             <th>
-                                                <?php _e( 'The height of code block:', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'The height of code block:', MSHIGHLIGHTER_TEXT ); ?>
                                             </th>
                                             <td>
                                                 <input type="text" name="mshighlighter_settings[block_height]" id="mshighlighter_settings[block_height]" size="4" value="<?php if ( !empty($options['block_height']) ) { echo $options['block_height']; } else { echo "300"; } ?>" >
@@ -273,29 +270,29 @@ function mshighlighter_render_submenu_page() {
                                         <tr valign='top'>
                                             <td></td>
                                             <td class='help-text'>
-                                                <?php _e( 'The height (in pixels) of code block. Default is 300px.', 'my-syntax-highlighter' ); ?>
+                                                <?php _e( 'The height (in pixels) of code block. Default is 300px.', MSHIGHLIGHTER_TEXT ); ?>
                                             </td>
                                         </tr>
 
 
                                     </table>
-                                    <?php submit_button( __( 'Save Changes', 'my-syntax-highlighter' ), 'primary', 'submit', true ); ?>
+                                    <?php submit_button( __( 'Save Changes', MSHIGHLIGHTER_TEXT ), 'primary', 'submit', true ); ?>
                                 </div>
                             </div>
 
                             <div class="postbox" id="Preview">
-                                <h3 class="title"><?php _e( 'Preview', 'my-syntax-highlighter' ); ?></h3>
+                                <h3 class="title"><?php _e( 'Preview', MSHIGHLIGHTER_TEXT ); ?></h3>
                                 <div class="inside">
-                                    <p><?php _e( 'Click "Save Changes" to update this preview.', 'my-syntax-highlighter' ); ?></p>
+                                    <p><?php _e( 'Click "Save Changes" to update this preview.', MSHIGHLIGHTER_TEXT ); ?></p>
                                     <textarea readonly id="mshighlighter" class="mshighlighter" language="html" name="mshighlighter"><?php echo $example; ?></textarea>
-                                    <p><?php _e( 'This is an example of HTML language.', 'my-syntax-highlighter' ); ?></p>
+                                    <p><?php _e( 'This is an example of HTML language.', MSHIGHLIGHTER_TEXT ); ?></p>
                                 </div>
                             </div>
 
                             <div class="postbox" id="Using">
-                                <h3 class="title"><?php _e( 'Using', 'my-syntax-highlighter' ); ?></h3>
+                                <h3 class="title"><?php _e( 'Using', MSHIGHLIGHTER_TEXT ); ?></h3>
                                 <div class="inside">
-                                    <p><?php _e( 'Just switch to the Text/HTML editor and wrap your source code in one of the supported shortcodes (like `[code]...[/code]` that is universal shortcode) and this plugin takes care of the rest. Example:', 'my-syntax-highlighter' ); ?></p>
+                                    <p><?php _e( 'Just switch to the Text/HTML editor and wrap your source code in one of the supported shortcodes (like `[code]...[/code]` that is universal shortcode) and this plugin takes care of the rest. Example:', MSHIGHLIGHTER_TEXT ); ?></p>
 <pre><code>[code]
  This
 
@@ -303,10 +300,10 @@ function mshighlighter_render_submenu_page() {
 
  an "example"!
  [/code]</code></pre>
-                                    <p><?php _e( 'In this case, the shortcode will prevent WordPress from inserting paragraph breaks between `This`, `is` and `an "example"`, as well as ensure that the double quotes around `example` are not converted to typographic (curly) quotes.', 'my-syntax-highlighter' ); ?></p>
-                                    <p><?php _e( 'You can use shortcodes such as:', 'my-syntax-highlighter' ); ?><br>
-                                    <span><?php _e( '[code] [php] [javascript] [js] [xml] [html] [css] [scss] [less] [sass] [markdown] [perl] [sql] [mysql] [shell] [bash]', 'my-syntax-highlighter' ); ?></span></p>
-                                    <p><?php _e( 'NOTE: To avoid problems, edit posts that contain your source code only in Text/HTML mode.', 'my-syntax-highlighter' ); ?></p>
+                                    <p><?php _e( 'In this case, the shortcode will prevent WordPress from inserting paragraph breaks between `This`, `is` and `an "example"`, as well as ensure that the double quotes around `example` are not converted to typographic (curly) quotes.', MSHIGHLIGHTER_TEXT ); ?></p>
+                                    <p><?php _e( 'You can use shortcodes such as:', MSHIGHLIGHTER_TEXT ); ?><br>
+                                    <span><?php _e( '[code] [php] [javascript] [js] [xml] [html] [css] [scss] [less] [sass] [markdown] [perl] [sql] [mysql] [shell] [bash]', MSHIGHLIGHTER_TEXT ); ?></span></p>
+                                    <p><?php _e( 'NOTE: To avoid problems, edit posts that contain your source code only in Text/HTML mode.', MSHIGHLIGHTER_TEXT ); ?></p>
                                 </div>
                             </div>
 
