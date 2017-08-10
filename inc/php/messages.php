618296854a3d3401ce9c3eaa949b34f2f6d84a01
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-08-10 16:48:37 +0300

    Plugin updated to version 2.7

--- inc/php/messages.php
+++ inc/php/messages.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) or die( "Restricted access!" );
 /**
  * Hello message - Bootstrap Modal
  *
- * @since 2.6
+ * @since 2.7
  */
 function mshighlighter_hello_message() {
 
@@ -25,7 +25,7 @@ function mshighlighter_hello_message() {
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body">
-                        <img src="https://www.spacexchimp.com/assets/dynamic-content/images/avatar.png">
+                        <img src="<?php echo MSHIGHLIGHTER_URL . 'inc/img/avatar.png'; ?>">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <p><?php _e( 'Hello. I\'m Arthur, the author of this plugin.', MSHIGHLIGHTER_TEXT ); ?></p>
                         <p><?php printf(
