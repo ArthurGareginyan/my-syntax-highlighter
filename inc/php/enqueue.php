052fca2c589f59f461211bfe0816abf4046a8d03
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-09-23 05:13:21 +0300

    Plugin updated to version 2.13

--- inc/php/enqueue.php
+++ inc/php/enqueue.php
@@ -88,7 +88,7 @@ function spacexchimp_p010_load_scripts_admin( $hook ) {
     $settings = SPACEXCHIMP_P010_SETTINGS;
 
     // Return if the page is not a settings page of this plugin
-    $settings_page = 'space-x-chimp_page_spacexchimp/' . $slug;
+    $settings_page = 'settings_page_' . $slug;
     if ( $settings_page != $hook ) return;
 
     // Read options from database
