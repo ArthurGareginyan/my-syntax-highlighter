9cfdc2699b6e68129f9f0e1571efeb5303f074a8
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-10-03 20:21:11 +0300

    New feature: Restoring screen position after saving changes

--- inc/php/tabs/settings.php
+++ inc/php/tabs/settings.php
@@ -116,6 +116,14 @@ defined( 'ABSPATH' ) or die( "Restricted access!" );
                         </div>
                     </div>
 
+                    <!-- HIDDEN -->
+                    <?php
+                        spacexchimp_p010_control_hidden( 'hidden_scrollto',
+                                                         '0'
+                                                       );
+                    ?>
+                    <!-- END HIDDEN -->
+
                     <!-- SUBMIT -->
                     <input type="submit" name="submit" id="submit" class="btn btn-default btn-lg button-save-main" value="<?php _e( 'Save changes', $plugin['text'] ); ?>">
                     <!-- END SUBMIT -->
